27d7b095a12e32b31a52e7e858141e35b1a3ca80
Jose Alonso <mail@josealonso.dev>
2024-01-26 16:33:56 -0600

    Get Pokemon for a online repository
    https://pokeapi.co/api/v2/pokemon

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return `Seed executed`;
+  private readonly axios: AxiosInstance;
+
+  async executeSeed() {
+    const maxPokemon = 10;
+
+    const { data } = await axios.get<PokeResponse>(
+      `https://pokeapi.co/api/v2/pokemon?limit=${maxPokemon}}`,
+    );
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = +(url.endsWith('/')
+        ? segments[segments.length - 2]
+        : segments[segments.length - 1]);
+    });
+
+    return data.results;
   }
 }
