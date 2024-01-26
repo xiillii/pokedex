7d2e45a40c2c24f499e8b229637c0fd441e9a21f
Jose Alonso <mail@josealonso.dev>
2024-01-26 14:57:52 -0600

    Implement MongoID Pipe to validate the Id to Delete

--- src/common/pipes/parse-mongo-id/parse-mongo-id.pipe.ts
+++ src/common/pipes/parse-mongo-id/parse-mongo-id.pipe.ts
@@ -1,8 +1,18 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { isValidObjectId } from 'mongoose';
 
 @Injectable()
 export class ParseMongoIdPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
+    if (!isValidObjectId(value)) {
+      throw new BadRequestException(`${value} is not a valid Mongo Id`);
+    }
+
     return value;
   }
 }
