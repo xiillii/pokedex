e366f8164679ac3549faba9a8621c11a42e20626
Jose Alonso <mail@josealonso.dev>
2024-01-26 20:53:18 -0600

    Load Config Configuration

---
+++ src/config/env.config.ts
@@ -0,0 +1,7 @@
+export const EnvConfiguration = () => ({
+  environment: process.env.NODE_ENV || 'dev',
+  mongodb: process.env.MONGODB,
+  runport: process.env.RUN_PORT || 3002,
+  queryDefaultLimit: +process.env.QUERY_DEFAULT_LIMIT || 10,
+  queryDefaultOffset: +process.env.QUERY_DEFAULT_OFFSET || 0,
+});
