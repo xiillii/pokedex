31af93b8586cbf3a9c2553117e9c73c233a4c730
Jose Alonso <mail@josealonso.dev>
2024-01-26 20:43:02 -0600

    Add config package and .ENV file for environment variables
    yarn add @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule,
@@ -15,4 +17,8 @@ import { SeedModule } from './seed/seed.module';
     SeedModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(process.env);
+  }
+}
